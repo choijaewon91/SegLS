feb3d1b41dcfce4e874b1b8ddce93dcb24230599
choijaewon91 <choijaewon91@gmail.com>
2021-04-30 10:05:23 -0500

    Debugged RLS_SUB
    Fixed issue with error computation( using posteriori residual -> prediction error)

--- RLS_SUB.py
+++ RLS_SUB.py
@@ -21,7 +21,7 @@ def RLS_step(x, d, P, w, dd, Xd, alpha):
     [lse, g, ret_P, ret_dd, ret_Xd] =  LSE_step(x, d, P, dd, Xd, alpha)
     e = d-w.dot(x)
     ret_w = w+e*g
-    e = (1 - g.dot(x))*e
+    #e = (1 - g.dot(x))*e
     
     return ret_w, e, lse, ret_P, ret_dd, ret_Xd
     
@@ -115,7 +115,7 @@ def RLS_batch(x, d, p, alpha, x0=None, P0 = None, w0 = None):
     return w[1:,:], e, lse
     
 
-def Segmented_LS(x, d, p, seg_penalty, alpha, x0 = None, verbose = False):
+def Segmented_LS(x, d, p, seg_penalty, x0 = None, verbose = False):
     if(len(x) != len(d)):
         print("RLS_batch: Current version only allows len(x)=len(d)")    
     
@@ -141,12 +141,62 @@ def Segmented_LS(x, d, p, seg_penalty, alpha, x0 = None, verbose = False):
     
     ##PART2 get filter coeff and error
     seg_start_idx = 0
-    A_r = np.zeros((p,))
-    if(x0 is not None):
-        A_r = x0[:]
-    #w = np.zeros((len(seg),p))
     w = np.zeros((N,p))
     e = np.zeros((N,))
+    dhat = np.zeros((N,))
+    x_init = np.zeros((p,))
+    if(x0 is not None):
+        x_init[:] = x0[:]
+        
+    for i in np.arange(1,len(seg)):
+        seg_start_idx = seg[i-1]
+        seg_end_idx = seg[i]
+        print(seg_start_idx)
+            
+        [w[seg_start_idx:seg_end_idx,:], e[seg_start_idx:seg_end_idx], _] = RLS_batch(x[seg_start_idx:seg_end_idx], d[seg_start_idx:seg_end_idx], p,1,x0=x_init)
+        w[seg_start_idx:seg_end_idx,:]=w[seg_end_idx-1,:]
+        x_init[:] = x[seg_end_idx-1:seg_end_idx-1-p:-1]
+    x_arr = np.zeros((p,))
+    if(x0 is not None):
+        x_arr[:] = x0[:]
+    for i in np.arange(N):
+        x_arr[1:] = x_arr[:-1]
+        x_arr[0] = x[i]
+        dhat[i] = w[i,:].dot(x_arr)
+        e[i] = d[i]-dhat[i]
+    return seg, w, e, dhat
+
+def Segmented_RLS(x, d, p, seg_penalty, alpha, x0 = None, verbose = False):
+    if(len(x) != len(d)):
+        print("RLS_batch: Current version only allows len(x)=len(d)")    
+    
+    N = len(x)
+    ##PART1. Find segments
+    x_init = np.zeros((p,))
+    if(x0 is not None):
+        x_init[:] = x0[:]
+    
+    E = np.zeros((N,N))
+
+    for i in np.arange(N):
+        
+        E[i,i:] = LSE_batch(x[i:], d[i:], p, x0 = x_init)
+        x_init[1:] = x_init[:-1]
+        x_init[0] = x[i]
+        
+        if(i%100 == 0 and verbose):
+            print('Segmented_LS - E iter: ' + str(i))
+            
+    [M, MI] = Segmented_LS_Bellman_batch(E, seg_penalty)
+    seg = Segmented_LS_Find_Segment(MI)
+    
+    ##PART2 get filter coeff and error
+    seg_start_idx = 0
+    
+    w = np.zeros((N,p))
+    e = np.zeros((N,))
+    dhat = np.zeros((N,))
+
     x_init = np.zeros((p,))
     if(x0 is not None):
         x_init[:] = x0[:]
@@ -155,22 +205,8 @@ def Segmented_LS(x, d, p, seg_penalty, alpha, x0 = None, verbose = False):
         seg_end_idx = seg[i]
         [w[seg_start_idx:seg_end_idx,:], e[seg_start_idx:seg_end_idx], _] = RLS_batch(x[seg_start_idx:seg_end_idx], d[seg_start_idx:seg_end_idx], p, alpha, x0 = x_init)
         x_init[:] = x[seg_end_idx-1:seg_end_idx-1-p:-1]
-        
-        # A_r[1:] = A_r[:-1]
-        # A_r[0] = x[seg_start_idx]
-        
-        # A_c = x[seg_start_idx:seg_end_idx]
-        # A = sp.linalg.toeplitz(A_c,A_r)
-        # w[i,:] = np.linalg.lstsq(A,d[seg_start_idx:seg_end_idx],rcond = None)[0]
-        # e[seg_start_idx:seg_end_idx] = d[seg_start_idx:seg_end_idx] - np.convolve( np.concatenate((A_r[p-1:0:-1],A_c)), w[i,:], mode = 'valid' )
-        
-        
-        # A_r[:] = x[seg_end_idx-1:seg_end_idx-p-1:-1 ]
-            
-        
-    return seg, w, e
 
-
+    return seg, w, e, dhat
 def Sequential_Segmented_RLS(x, d, p, seg_penalty, seg_threshold, alpha, x0=None, P0 = None, w0 = None, verbose = False):
     if(len(x) != len(d)):
         print("RLS_batch: Current version only allows len(x)=len(d)")    
@@ -198,6 +234,7 @@ def Sequential_Segmented_RLS(x, d, p, seg_penalty, seg_threshold, alpha, x0=None
     seg = np.zeros((N,),dtype='int32')
     N_seg = 1
     seg_start = seg[0]
+    PP = np.eye(p)
     for j in np.arange(N):
         x_arr[1:] = x_arr[:-1]
         x_arr[0] = x[j]
@@ -207,19 +244,22 @@ def Sequential_Segmented_RLS(x, d, p, seg_penalty, seg_threshold, alpha, x0=None
         if((j-seg_start) > 1 and  (MI[j-1]-MI[j-2] >  seg_threshold) ):
             seg_start = j
             seg[N_seg] = seg_start
+            PP[:,:]=P[MI[j-1],:,:]
             N_seg+=1
+            
+            
         #information from segmented LS        
         for i in np.arange(j,seg_start-1,-1):#np.arange(j+1):     
-            [E[i,j], g_0, P[i,:,:], dd[i], Xd[i,:]] = LSE_step(x_arr, d[j], P[i,:,:], dd[i], Xd[i,:], 0.99)
+            [E[i,j], g_0, P[i,:,:], dd[i], Xd[i,:]] = LSE_step(x_arr, d[j], P[i,:,:], dd[i], Xd[i,:], 1)
             if(i==seg_start):
                 g = g_0
         
         [M[j], MI[j]] = Segmented_LS_Bellman_step(E[seg_start:j+1,j], seg_penalty, M[seg_start:j])
         MI[j]=MI[j]+seg_start
-        #[M[j], MI[j]] = Segmented_LS_Bellman_step(E[:j+1,j], seg_penalty, M[:j])
 
-        e[j] = d[j]-w[j,:].dot(x_arr)
-        w[j+1,:] = w[j,:]+e[j]*g
-        e[j] = (1 - g.dot(x_arr))*e[j]
+        # e[j] = d[j]-w[j,:].dot(x_arr)
+        # w[j+1,:] = w[j,:]+e[j]*g
+        # e[j] = (1 - g.dot(x_arr))*e[j]
+        [w[j+1,:], e[j], _, PP, _, _] = RLS_step(x_arr, d[j], PP, w[j,:], dd[j], Xd[j,:], alpha)
     seg[N_seg] = N
     return w[1:,:], e, seg[:N_seg+1]
--- SegLS.py
+++ SegLS.py
@@ -5,105 +5,9 @@ from scipy.io import loadmat
 import RLS_SUB as RS
 
 import matplotlib.pyplot as plt
-
-def lpc(y, p):
-    c = np.correlate(y,y,mode = 'full')
-    c = c[len(y)-1:]
-    r = np.array(c)
-    r[1:] = np.conj(r[1:])
     
-    a = sp.linalg.solve_toeplitz((c[:p],r[:p]),-c[1:p+1])
-    
-    # R = np.linalg.toeplitz(c[:p],r[:p])
-    
-    
-    b = np.ones((len(y)-1,))
-    b[:] = np.convolve(y,a)[:len(y)-1]
-    y_z = np.zeros((len(y)-1,))
-    y_z[:] = y[1:]
-    #g = np.sum((y_z[:]-b)**2)/(len(y))
-    g = np.var(y_z[:]+b)
-    #g = np.var( sp.linalg.solve_toeplitz(c[:p],r[:p]).dot(a)+c[1:p+1] )
-    
-    a = np.concatenate(([1],a))
-    return a,g
-        
-def lpc_rls(y,p,alpha, y0 = None, P = None):
-    w = np.zeros((p,))
-    x_in = np.zeros((p,))
-    
-    if(P is None):
-        P = y[0]**2*np.eye(p)
-    if(y0 is not None):
-        x_in[:] = y0
-        
-    e = np.zeros((len(y),))
-    e_var = np.zeros((len(y),))
-    
-    e[0] = y[0] - x_in.dot(w)    
-    e_var[0] = e[0]**2
-    
-    y_sqrd_sum = y[0]**2
-    Ab = np.zeros((p,))
-    for i in np.arange(1,len(y)):
-        x_in[1:] = x_in[0:-1]
-        x_in[0] = y[i-1]
-         
-        e[i] = y[i] - x_in.dot(w)
-        
-        Px = P.dot(x_in)
-        g = Px/(alpha+x_in.dot(Px))
-        w[:] = w[:] + e[i]*g[:]
-        
-        y_sqrd_sum += y[i]**2
-        Ab += y[i]*x_in[:]
-        
-        
-        P[:,:] = 1/alpha * (P[:,:]-np.outer(g,Px)) 
-        P = 1/2*(P.T+P)
-        
-        e_var[i] = y_sqrd_sum - Ab.dot(P.dot(Ab))
-    return w, e_var
-
-
-def recursive_MMSE(y_in, p):
-    N = len(y_in)
-    
-    A_r = np.zeros((N-1,))
-    A_r[:] = y_in[0:-1]
-    A_c = np.zeros((p,))
-    A_c[0] = y_in[0]
-    A = sp.linalg.toeplitz(A_r, A_c)
-    var_e = np.zeros((N,)) 
-    var_e[0]= y_in[0]**2
-    
-    
-    y = y_in[1:]
-    for i in np.arange(N-1):
-        if(i<p-1):
-             var_e[i+1] = var_e[i]+y[i]**2
-        elif(i==p-1):
-            P = np.linalg.lstsq( A[:i+1,:].T.dot(A[:i+1,:]), np.eye(p), rcond=None )[0]
-            err_term1 =  y[:i+1].dot(y[:i+1])
-            err_term2 =  A[:i+1,:].T.dot( y[:i+1])
-            var_e[i+1] = err_term1 - err_term2.dot(P.dot(err_term2))
-        else:
-            Py = P.dot(A[i,:])
-            ATb = A[:i+1,:].T.dot(y[:i+1])
-
-            newP = P - 1/(1+A[i,:].dot(Py))*(np.outer(Py,Py))
-            newP = 1/2*(newP.T+newP)
-                       
-            err_term1 += y[i]**2
-            err_term2 = ATb.dot( (newP).dot(ATb) )
-            
-            P = newP
-            var_e[i+1] = err_term1 - err_term2
-        
-    return var_e
-    
-
-x = np.random.randn(256)
+XL = 512
+x = np.random.randn(XL)
 ###################for debugging
 # from_matlab= loadmat('data.mat')
 # x= from_matlab['x'].flatten()
@@ -111,20 +15,28 @@ x = np.random.randn(256)
 plt.figure()
 plt.plot(x)
 plt.show()
-a=np.array([[1, -0.9, 0.4, -0.1],
-            [1, 0.9, 0.2, 0.2],
-            [1, -0.99, 0.5, 0.3]])
-# a=np.array([[1, -0.9, 0.4],
-#             [1, 0.9, 0.2],
-#             [1, -0.99, 0.5]])
+# a=np.array([[1, -0.9, 0.4, -0.1],
+#             [1, 0.9, 0.2, 0.2],
+#             [1, -0.99, 0.5, 0.3]])
+a=np.array([[1, -0.9, 0.4],
+            [1, 0.9, -0.05],
+            [1, -0.99, 0.5],
+            [1, 0.9, -0.05]])
 
 # a=np.array([[1, -0.9],
 #             [1, 0.9],
 #             [1, -0.99]])
+# a=np.array([[1, -0.9]])
 y= np.array([])
 print('True partition:')
+NN = np.zeros((np.shape(a)[0]+1,),dtype='int32')
+NN[0] = 0
+NNI = 1
 for i in np.arange(np.shape(a)[0]):
+    x = np.random.randn(XL)
     y = np.concatenate((y,signal.lfilter([1.0],a[i,:],x)) )
+    NN[NNI] = len(y)
+    NNI+=1
     print('    '+str(len(y)))    
 plt.figure()
 plt.plot(y.flatten())
@@ -132,150 +44,105 @@ plt.show()
 
 
 N = len(y)
-mo = 3
+mo = 2
 C = np.zeros((1,mo))
 R= np.eye(mo)
 alpha = 0.99
 ahat=np.zeros((mo,N))
 yhat = np.zeros((N,))
-#%%RLS
-# for i in np.arange(N):
-#     if( i >= mo ):
-#         C[:] = y[i-mo:i].reshape((1,-1))
-#     else:
-#         C[:] = np.zeros((1,mo))
 
-#     R = alpha*R + C.T.dot(C)
-
-#     if(i>=mo-1):
-#         yhat[i] = np.dot(C,ahat[:,i-1])
-#         ahat[:,i] = ahat[:,i-1]+np.linalg.lstsq(R,C.flatten()*(y[i]-yhat[i]),rcond=None)[0]
-#     else:
-#         yhat[i] = 0
-#         ahat[:,i] = np.linalg.lstsq( R,C.flatten()*(y[i]-yhat[i]) ,rcond=None)[0]
-
-#     if(i%100 == 0):
-#         print('RLS iter:' + str(i))
-# plt.figure()
-# for i in np.arange(mo):
-#     plt.plot(ahat[i,:])
-# plt.show()
-
-[w, e_rls, lse_rls]= RS.RLS_batch( np.concatenate(([0], y[0:-1])), y[:], mo, alpha)
-
+[ahat1, e_rls, lse_rls]= RS.RLS_batch( np.concatenate(([0], y[0:-1])), y[:], mo, alpha)
+x_arr= np.zeros((mo,))
+lse_rls = np.cumsum(e_rls[:]**2)
 plt.figure()
 for i in np.arange(mo):
-    plt.plot(w[:,i])
+    plt.plot(ahat1[:,i])
 plt.show()
 
-#%%Calculate the pair-wise errors
-E = np.zeros((N,N))
-Const = (mo+1)*np.var(y)
-# Using RLS lpc
-for i in np.arange(N):
-    # [aa, g] = lpc_rls(y[i:],mo,1)
-    # # g = recursive_MMSE(y[i:], mo)
-    # E[i,i:i+mo] = np.cumsum(y[i:i+mo]**2)
-    # E[i,i+mo:] = g[mo:]
-    
-    E[i,i:] = RS.LSE_batch(np.concatenate(([0], y[i:-1])), y[i:], mo)
-    if(i%100 == 0):
-        print('E iter: ' + str(i))
-#MY SLS
+#%%sls
+Const = (mo+1)**2*np.var(y)
 
-[seg, w, e] = RS.Segmented_LS(np.concatenate(([0], y[:-1])), y[:], mo, Const,alpha)
+[seg, ahat_sls,e_sls,dhat1] = RS.Segmented_LS(np.concatenate(([0], y[:-1])), y[:], mo, Const)
 
 MI = np.zeros((N,))
-print('Segmented RLS partition')
+print('Segmented LS partition')
 for i in np.arange(1,len(seg)):
     MI[seg[i-1]:seg[i]]=seg[i-1]
     print('    '+ str(seg[i]))
 
-sls_lse = np.cumsum(e**2)
+sls_lse = np.cumsum(e_sls[:]**2)
 
-    
+#%%srls
 
+[seg, ahat_srls,e_srls,dhat2] = RS.Segmented_RLS(np.concatenate(([0], y[:-1])), y[:], mo, Const,alpha)
 
-
-
-#Seguentially achievable:
-T = np.zeros((N,))
-TI = np.zeros((N,),dtype = 'int32')
-MM = np.zeros((N,))
-MMI = np.zeros((N,),dtype = 'int32')
-# Const = 1/mo*Const
-
-# for i in np.arange(N):
-#     for j in np.arange(i+1):
-#         cost = E[:j+1,j] + Const + np.concatenate(([0],T[:j]) )
-#         TI[j] = np.argmin(cost)
-#         T[j] = cost[TI[j]]
-#     MM[i]=T[i]
-#     MMI[i]=TI[i]
-[MM, MMI] = RS.Segmented_LS_Bellman_batch(E, Const)
-
-
-#%%segRLS-reset RLS
-alpha = 0.99
-ahat2 = np.zeros((mo,N))
-yhat2 = np.zeros((N,))
-R = np.eye(mo)
-seg = np.zeros((N,))
-seg_idx = 0
-for i in np.arange(N):
-    if(i>=mo):
-        C = y[i-mo:i].reshape((1,-1))
-    else:
-        C = np.zeros((1,mo))
-
-    if(i > 0 and ( (MMI[i]-MMI[i-1]) > 20 ) ):
-        seg[seg_idx]=i
-        seg_idx+=1
-        R = C.T.dot(C)+np.eye(mo)
-    else:
-        R = alpha*R + C.T.dot(C)
-
-    if(i >= mo-1):
-        yhat2[i] = C.dot(ahat2[:,i-1])
-        ahat2[:,i] = ahat2[:,i-1]+np.linalg.lstsq(R, C.flatten()*(y[i]-yhat2[i]),rcond=None)[0]
-    else:
-        yhat2[i] = 0
-        ahat2[:,i] = np.linalg.lstsq(R, C.flatten()*(y[i]-yhat2[i]),rcond=None)[0]
-    
-    if(i%100 == 0):
-        print('RLS2 iter: ' + str(i))
-        
-print('Sequential RLS partition')
-for i in np.arange(seg_idx):
+MMI = np.zeros((N,))
+print('Segmented RLS partition')
+for i in np.arange(1,len(seg)):
+    MMI[seg[i-1]:seg[i]]=seg[i-1]
     print('    '+ str(seg[i]))
 
+srls_lse = np.cumsum(e_srls[:]**2)    
+#srls_lse = np.cumsum((y[:]-dhat2)**2)    
 
-[ahat2, SSLS_e, SSLS_seg]= RS.Sequential_Segmented_RLS(np.concatenate(([0], y[:-1])), y[:], mo, Const, 20, alpha)
-SSLS_lse = np.cumsum(SSLS_e**2)
+#%%
+[ahat3, SSLS_e, SSLS_seg]= RS.Sequential_Segmented_RLS(np.concatenate(([0], y[:-1])), y[:], mo, Const, 20, alpha)
+SSLS_lse = np.cumsum(SSLS_e[:]**2)
 MMMI = np.zeros((N,))
 print('Sequential RLS partition - version J')
 for i in np.arange(1,len(SSLS_seg)):
     MMMI[SSLS_seg[i-1]:SSLS_seg[i]]=SSLS_seg[i-1]
     print('    '+ str(SSLS_seg[i]))
     
+    
+    
+    #%%plotting
 plt.figure()
-plt.plot(np.cumsum((y-yhat)**2)/np.arange(1,N+1),label = 'RLS memory 0.99')
-plt.plot(np.cumsum((y-yhat2)**2)/np.arange(1,N+1),label = 'Segmented RLS-informed RLS memory 0.99')
-plt.plot(sls_lse/np.arange(1,N+1),label = 'Optimal Segmented LS' )
-plt.plot(SSLS_lse/np.arange(1,N+1), label = 'Segmented RLS-informed RLS memory 0.99 - version J')
+plt.plot(SSLS_lse/np.arange(1,N+1),'-y', label = 'sequential segmented RLS')
+plt.plot(sls_lse/np.arange(1,N+1),'-.b',label = 'Optimal Segmented LS' )
+plt.plot(srls_lse/np.arange(1,N+1),'--r',label = 'segmented RLS' )
+plt.plot(lse_rls/np.arange(1,N+1),':g',label = 'RLS memory 0.99')
+
 plt.legend()
+plt.title('Mean Squared Accumulated Prediction Error')
 plt.xlabel('Samples')
-plt.ylabel('Mean Squared Accumulated Prediction Error')
+plt.ylabel('Mean Squared Error')
 plt.show()
 
 
 plt.figure()
-plt.plot(y, label = 'switching AR(2) process' )
-plt.plot(MI/50,label = 'optimal segmented least squares')
-plt.plot(-MMI/50, label = 'sequential segmented least squares')
-plt.plot(-MMMI/50, label = 'sequential segmented least squares - version J')
+plt.plot(y, label = 'switching AR process' )
+plt.axvline(x = NN[0],color = 'k',label = 'true partition')
+plt.plot(-MMMI/50, '-y', label = 'sequential segmented RLS')
+plt.plot(MI/50,'--r',label = 'segmented LS/RLS')
+
+
+for i in np.arange(1,np.shape(a)[0]):
+    plt.axvline(x = NN[i],color = 'k')
+
 plt.legend()
+plt.title("Segmentation of the Piecewise Stationary Sequence")
 plt.xlabel('Samples')
-plt.ylabel('piecewise stationary sequence / optimal segments')
+plt.ylabel('Piecewise Stationary Sequence / Partition index x0.2')
+plt.show()
+
+
+a_coeff = np.zeros((N,mo))
+for i in np.arange(np.shape(a)[0]):
+    a_coeff[NN[i]:NN[i+1],:]=a[i,1:]    
+
+#%%
+plt.figure()
+for i in np.arange(mo):
+    plt.subplot(mo,1,i+1)
+    plt.plot(ahat3[:,i], '-y',label = 'sequential segmented RLS')
+    plt.plot(ahat_sls[:,i], '-.b',label = 'optimal segmented LS')
+    plt.plot(ahat_srls[:,i], '--r', label = 'segmented RLS')
+    plt.plot(ahat1[:,i],    ':g',label = 'RLS memory 0.99')
+    
+    plt.plot(-a_coeff[:,i],'-k',label = 'True Coefficient')
+plt.suptitle('Filter Coefficient')
+plt.legend(loc='upper center',bbox_to_anchor = (0.5, -0.35), ncol = 2)
+plt.xlabel('Samples')
 plt.show()
 
